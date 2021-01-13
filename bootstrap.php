f73dfe2c048c267c0ad26a65fe8f63573f755d9c
Valentin Krebs <valentin@vkrebs.fr>
2021-01-13 18:55:07 +0100

    Deploiement

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ use Doctrine\ORM\EntityManager;
 date_default_timezone_set('America/Lima');
 require_once __DIR__ . '/vendor/autoload.php';
 $isDevMode = true;
-$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "\app\Config\Yaml"), $isDevMode);
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/app/Config/Yaml"), $isDevMode);
 $conn = array(
     'host' => 'ec2-34-254-24-116.eu-west-1.compute.amazonaws.com',
     'driver' => 'pdo_pgsql',

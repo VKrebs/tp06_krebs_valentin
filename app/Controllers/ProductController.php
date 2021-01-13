3071918e654a9eb5999e3d89366e3b9cb7ad3bc2
Valentin Krebs <valentin@vkrebs.fr>
2021-01-13 10:24:03 +0100

    bug fix

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controllers;
 
-include_once ('./../bootstrap.php');
+include_once (__DIR__."/../../bootstrap.php");
 use App\Models\Product;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
